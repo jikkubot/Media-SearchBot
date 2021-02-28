97e99398711cd486b5c2ec8bd88ebdd0343bec69
dulquer9 <73124813+dulquer9@users.noreply.github.com>
2021-02-28 18:33:46 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -30,7 +30,7 @@ async def answer(bot, query):
             InlineQueryResultCachedDocument(
                 title=file.file_name,
                 file_id=file.file_id,
-                caption=file.caption or "",
+                caption=file.caption or "📢: @TGFilmBot",
                 description=f'Size: {get_size(file.file_size)}\nType: {file.file_type}',
                 reply_markup=reply_markup))
 
